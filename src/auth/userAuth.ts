5dda3b16748018c8030198ad523352b6ee3f7f67
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2022-02-06 17:56:02 +0100

    Lowered access token expiration and removed unecessary step in CI

--- src/auth/userAuth.ts
+++ src/auth/userAuth.ts
@@ -76,7 +76,7 @@ export async function verifyAndFetchAllUser(request: FastifyRequest, reply: Fast
 
 
 export async function generateAccessToken(payload: IUserAccessToken) {
-    return jwt.sign(payload, access, { expiresIn: "30m" });
+    return jwt.sign(payload, access, { expiresIn: "30s" });
 }
 export async function generateRefreshToken(payload: IUserRefreshToken) {
     return jwt.sign(payload, refresh);
